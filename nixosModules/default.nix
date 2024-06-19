6644edd99a7225efe7ca8d54727adf9f790589f1
Andy3153 <andy3153@protonmail.com>
2024-06-19 17:44:09 +0300

    modules/extraPackages ; modules/extraFlatpakPackages : init

--- nixosModules/default.nix
+++ nixosModules/default.nix
@@ -3,8 +3,39 @@
 ## Custom NixOS modules bundle
 ##
 
-{ ... }:
+{ config, lib, ... }:
 
+let
+  cfg = config.custom;
+
+  # {{{ Flatpak package options
+  packageOptions = _:
+  {
+    options =
+    {
+      appId = lib.mkOption
+      {
+        type        = lib.types.str;
+        description = "the app ID of the app to install";
+      };
+
+      commit = lib.mkOption
+      {
+        type        = lib.types.nullOr lib.types.str;
+        default     = null;
+        description = "hash of the app commit to install";
+      };
+
+      origin = lib.mkOption
+      {
+        type        = lib.types.str;
+        default     = "flathub";
+        description = "repository origin (default: flathub)";
+      };
+    };
+  };
+  # }}}
+in
 {
   imports =
   [
@@ -19,4 +50,30 @@
     ./virtualisation
     ./xdg
   ];
+
+  options.custom =
+  {
+    extraPackages = lib.mkOption
+    {
+      type        = lib.types.listOf lib.types.package;
+      default     = [ ];
+      description = "extra packages to add to the configuration";
+    };
+
+    extraFlatpakPackages = lib.mkOption
+    {
+      type        = with lib.types; listOf (coercedTo str (appId: { inherit appId; }) (submodule packageOptions));
+      default     = [ ];
+      description = "extra flatpak packages to add to the configuration";
+    };
+  };
+
+  config =
+  {
+    custom =
+    {
+      environment.systemPackages = cfg.extraPackages;
+      services.flatpak.packages  = cfg.extraFlatpakPackages;
+    };
+  };
 }
