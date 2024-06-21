92364db8f4d40599abce8ab8f7be17c4cb06bb33
Andy3153 <andy3153@protonmail.com>
2024-06-22 02:51:51 +0300

    modules/hardware/openrgb: override to latest master, fixes logitech g203 not detected

--- nixosModules/hardware/openrgb.nix
+++ nixosModules/hardware/openrgb.nix
@@ -7,6 +7,28 @@
 
 let
   cfg = config.custom.hardware.openrgb;
+
+  # {{{ OpenRGB derivation
+  openrgbPackage = pkgs.openrgb-with-all-plugins.overrideAttrs (_:
+  {
+    version = "0.91";
+
+    src = pkgs.fetchFromGitLab
+    {
+      owner  = "CalcProgrammer1";
+      repo   = "OpenRGB";
+      rev    = "15c3d8591c177b04aa788605607a2c7f8a5b049e";
+      sha256 = "sha256-SLu738Rpb4QvV+zBRnOYcMdE4uc3IQF1X+CgOqEkf4o=";
+    };
+
+    postPatch =
+    ''
+      patchShebangs scripts/build-udev-rules.sh
+      substituteInPlace scripts/build-udev-rules.sh \
+        --replace "/usr/bin/env chmod" "${lib.getExe' pkgs.coreutils "chmod"}"
+    '';
+  });
+  # }}}
 in
 {
   options.custom.hardware.openrgb.enable = lib.mkEnableOption "enables OpenRGB";
@@ -15,7 +37,13 @@ in
   {
     custom.hardware.i2c.enable = lib.mkForce true; # force enable custom i2c
 
-    services.udev.packages           = [ pkgs.openrgb ];
-    services.hardware.openrgb.enable = lib.mkDefault true;
+    #environment.systemPackages = [ pkgs.openrgb-plugin-effects ];
+    services.udev.packages     = [ openrgbPackage ];
+
+    services.hardware.openrgb =
+    {
+      enable  = lib.mkDefault true;
+      package = openrgbPackage;
+    };
   };
 }
