dfb4800de91402864db919f68866d624ead8ba29
Andy3153 <andy3153@protonmail.com>
2024-08-05 23:37:02 +0300

    modules/user/mainUser: i learned what the enum type is so i'll fix this

--- TODO.md
+++ TODO.md
@@ -1,5 +1,2 @@
-error out when users.mainUser is empty
 redo boot.efiBoot logic
 redo hardware.isLaptop logic
-separate nixpkgs stable and unstable better
-hunspellDicts.ro-RO is only in unstable
--- nixosModules/users/mainUser.nix
+++ nixosModules/users/mainUser.nix
@@ -8,7 +8,7 @@
 {
   options.custom.users.mainUser = lib.mkOption
   {
-    type        = lib.types.str;
+    type        = lib.types.enum [ "andy3153" "bot" ];
     description = "the main user of this configuration";
     default     = "";
     example     = "andy3153";
@@ -16,22 +16,6 @@
 
   config = lib.mkMerge
   [
-    {
-      warnings =
-        if (config.custom.users.mainUser == "")
-        then [ ''You did not set a value for `custom.users.mainUser`. This might break things.'' ]
-        else [];
-
-      assertions =
-      [
-        {
-          assertion = config.custom.users.mainUser != "";
-          message = "set a main user!!";
-        }
-      ];
-    }
-
-    (lib.mkIf (config.custom.users.mainUser == "")         { } )
     (lib.mkIf (config.custom.users.mainUser == "andy3153") { custom.users.andy3153.enable = true; })
     (lib.mkIf (config.custom.users.mainUser == "bot")      { custom.users.bot.enable      = true; })
   ];
