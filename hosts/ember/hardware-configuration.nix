199ca1303f96fd9e9185fb8c646b8dee2a5f9efd
Andy3153 <andy3153@protonmail.com>
2024-08-06 15:17:09 +0300

    hosts/ember: created swap lv

--- hosts/ember/hardware-configuration.nix
+++ hosts/ember/hardware-configuration.nix
@@ -39,7 +39,7 @@
     };
   };
 
-  swapDevices = [ ];
+  swapDevices = [ { device = "/dev/ember/swap"; } ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
