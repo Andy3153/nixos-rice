4187f05bd7b76a2b01fab073fb3cc54bd3966161
Andy3153 <andy3153@protonmail.com>
2024-08-07 13:30:55 +0300

    modules/networking/extraHosts: init

---
+++ nixosModules/networking/extraHosts.nix
@@ -0,0 +1,22 @@
+## vim: set fenc=utf-8 ts=2 sw=0 sts=0 sr et si tw=0 fdm=marker fmr={{{,}}}:
+##
+## Extra hosts config
+##
+
+{ config, options, lib, ... }:
+
+let
+  cfg = config.custom.networking.extraHosts;
+in
+{
+  options.custom.networking.extraHosts = lib.mkOption
+  {
+    type = options.networking.extraHosts.type;
+    description = "entries to be added to `/etc/hosts`";
+  };
+
+  config = lib.mkIf cfg.enable
+  {
+    networking.extraHosts = cfg;
+  };
+}
