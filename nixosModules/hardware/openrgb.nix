47bfc5773d7da91b2972f3a36bb80bacddab8b36
Andy3153 <andy3153@protonmail.com>
2024-08-31 14:51:24 +0300

    modules/hardware/openrgb: moved overrides to my-nixpkgs
    [my-nixpkgs](https://github.com/Andy3153/my-nixpkgs)

--- nixosModules/hardware/openrgb.nix
+++ nixosModules/hardware/openrgb.nix
@@ -3,32 +3,10 @@
 ## OpenRGB config
 ##
 
-{ config, lib, pkgs, ... }:
+{ config, lib, my-pkgs, ... }:
 
 let
   cfg = config.custom.hardware.openrgb;
-
-  # {{{ OpenRGB derivation
-  openrgbPackage = pkgs.openrgb-with-all-plugins.overrideAttrs (_:
-  {
-    version = "0.91";
-
-    src = pkgs.fetchFromGitLab
-    {
-      owner  = "CalcProgrammer1";
-      repo   = "OpenRGB";
-      rev    = "428355761e43d47b8a2082fb0c369b931cf5c717";
-      sha256 = "sha256-e3QEHfJUXX24PGkVwYhJUV2JJj9mqE1K+W0Q/HIdjAw=";
-    };
-
-    postPatch =
-    ''
-      patchShebangs scripts/build-udev-rules.sh
-      substituteInPlace scripts/build-udev-rules.sh \
-        --replace "/usr/bin/env chmod" "${lib.getExe' pkgs.coreutils "chmod"}"
-    '';
-  });
-  # }}}
 in
 {
   options.custom.hardware.openrgb.enable = lib.mkEnableOption "enables OpenRGB";
@@ -38,12 +16,12 @@ in
     custom.hardware.i2c.enable = lib.mkForce true; # force enable custom i2c
 
     #custom.extraPackages  = [ pkgs.openrgb-plugin-effects ];
-    services.udev.packages = [ openrgbPackage ];
+    services.udev.packages = [ my-pkgs.openrgb-git ];
 
     services.hardware.openrgb =
     {
       enable  = true;
-      package = openrgbPackage;
+      package = my-pkgs.openrgb-git;
     };
   };
 }
