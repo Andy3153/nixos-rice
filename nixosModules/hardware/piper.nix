ff608bc119a99c80274b72b15589127164d8d356
Andy3153 <andy3153@protonmail.com>
2024-08-31 14:52:16 +0300

    modules/hardware/piper: moved overrides to my-nixpkgs
    [my-nixpkgs](https://github.com/Andy3153/my-nixpkgs)

--- nixosModules/hardware/piper.nix
+++ nixosModules/hardware/piper.nix
@@ -3,53 +3,21 @@
 ## Piper & ratbagd config
 ##
 
-{ config, lib, pkgs, ... }:
+{ config, lib, my-pkgs, ... }:
 
 let
   cfg = config.custom.hardware.piper;
-
-  # {{{ ratbagd derivation
-  ratbagdPackage = pkgs.libratbag.overrideAttrs (_:
-  {
-    version = "0.18";
-
-    src = pkgs.fetchFromGitHub
-    {
-      owner = "libratbag";
-      repo  = "libratbag";
-      rev   = "1c9662043f4a11af26537e394bbd90e38994066a";
-      hash  = "sha256-IpN97PPn9p1y+cAh9qJAi5f4zzOlm6bjCxRrUTSXNqM=";
-    };
-  });
-  # }}}
-
-  # {{{ Piper derivation
-  piperPackage = pkgs.piper.overrideAttrs (_:
-  {
-    version = "0.8";
-
-    src = pkgs.fetchFromGitHub
-    {
-      owner = "libratbag";
-      repo  = "piper";
-      rev   = "93a5a004766e37a801940cc3317c4aea9d2a6cfd";
-      hash  = "sha256-a+HBxFYzRfcLPR7Ut+sFEh9Aj8b+Xbqhp6TGGCVbCb8=";
-    };
-
-    mesonFlags = [ "-Druntime-dependency-checks=false" ];
-  });
-  # }}}
 in
 {
   options.custom.hardware.piper.enable = lib.mkEnableOption "enables Piper & ratbagd";
 
   config = lib.mkIf cfg.enable
   {
-    custom.extraPackages = [ piperPackage ];
+    custom.extraPackages = [ my-pkgs.piper-git ];
     services.ratbagd =
     {
       enable  = true;
-      package = ratbagdPackage;
+      package = my-pkgs.libratbag-git;
     };
   };
 }
