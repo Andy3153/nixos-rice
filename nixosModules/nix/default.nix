03763d708b7b7cacd924702d2c7701fafe6c6950
Andy3153 <andy3153@protonmail.com>
2024-09-15 22:24:55 +0300

    modules/nix/package: init
    also use nix patched by in-nix as default package

--- nixosModules/nix/default.nix
+++ nixosModules/nix/default.nix
@@ -8,6 +8,7 @@
 {
   imports =
   [
+    ./package.nix
     ./unfreeWhitelist.nix
   ];
 }
---
+++ nixosModules/nix/package.nix
@@ -0,0 +1,21 @@
+## vim: set fenc=utf-8 ts=2 sw=0 sts=0 sr et si tw=0 fdm=marker fmr={{{,}}}:
+##
+## Nix package config
+##
+
+{ config, lib, options, inputs, pkgs, ... }:
+
+let
+  cfg = config.custom.nix.package;
+  sys = pkgs.stdenv.hostPlatform.system;
+in
+{
+  options.custom.nix.package = lib.mkOption
+  {
+    type        = options.nix.package.type;
+    default     = inputs.in-nix.packages.${sys}.default.patchNix pkgs.nix;
+    description = "Nix package";
+  };
+
+  config.nix.package = cfg;
+}
