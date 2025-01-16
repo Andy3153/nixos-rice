c11204b227398126c949bb6f1e205b67d3b1af8f
Andy3153 <andy3153@protonmail.com>
2025-01-16 20:17:13 +0200

    modules/programs/steam: don't run the gamescope session with prime render offload anymore

--- nixosModules/programs/steam.nix
+++ nixosModules/programs/steam.nix
@@ -24,17 +24,7 @@ in
       dedicatedServer.openFirewall = true;
       remotePlay.openFirewall      = true;
 
-      gamescopeSession = lib.mkIf cfg.gamescopeSession.enable
-      {
-        enable = true;
-        env = lib.mkIf nvPrime.enable
-        {
-          __NV_PRIME_RENDER_OFFLOAD          = "1";
-          __NV_PRIME_RENDER_OFFLOAD_PROVIDER = "NVIDIA-G0";
-          __GLX_VENDOR_LIBRARY_NAME          = "nvidia";
-          __VK_LAYER_NV_optimus              = "NVIDIA_only";
-        };
-      };
+      gamescopeSession.enable = cfg.gamescopeSession.enable;
     };
 
     custom =
