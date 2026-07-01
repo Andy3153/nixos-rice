e2b00f8943b2d675b38df068eb508e4744ac28e0
Andy3153 <andy3153@protonmail.com>
2026-07-01 14:17:05 +0300

    flake: update lanzaboote to 1.1.0

--- flake.nix
+++ flake.nix
@@ -51,13 +51,13 @@
     # {{{ Lanzaboote
     lanzaboote_unstable =
     {
-      url = "github:nix-community/lanzaboote/v1.0.0";
+      url = "github:nix-community/lanzaboote/v1.1.0";
       inputs.nixpkgs.follows = "nixpkgs_unstable";
     };
 
     lanzaboote_stable =
     {
-      url = "github:nix-community/lanzaboote/v1.0.0";
+      url = "github:nix-community/lanzaboote/v1.1.0";
       inputs.nixpkgs.follows = "nixpkgs_stable";
     };
     # }}}
