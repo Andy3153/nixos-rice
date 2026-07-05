70c4f05c78a9d8bec04dc06f9137d91ecfc7e4d8
Andy3153 <andy3153@protonmail.com>
2026-07-05 17:27:11 +0300

    modules/programs/dolphin: create settings option

--- modules/programs/dolphin.nix
+++ modules/programs/dolphin.nix
@@ -5,20 +5,81 @@
 
 { config, lib, pkgs, ... }:
 
+# {{{ Variables
 let
-  cfg = config.custom.programs.dolphin;
-  workspacePkg = pkgs.kdePackages.plasma-workspace;
+  cfg      = config.custom.programs.dolphin;
+  mainUser = config.custom.users.mainUser;
+
+  qtStyle   = config.custom.gui.theme.qt.style.name;
+
+  # {{{ Dolphin config
+  dolphinConf =
+  {
+    ContentDisplay =
+    {
+      DirectorySizeMode    = "ContentSize";
+      UsePermissionsFormat = "NumericFormat";
+    };
+
+    ContextMenu.ShowCopyMoveMenu = true;
+    DetailsMode.PreviewSize = 22;
+
+    General =
+    {
+      AlwaysShowTabBar            = true;
+      GlobalViewProps             = false;
+      ShowStatusBar               = "FullWidth";
+      ShowToolTips                = true;
+      ShowZoomSlider              = true;
+      UseTabForSwitchingSplitView = true;
+    };
+
+    "KFileDialog Settings" =
+    {
+      "Places Icons Auto-resize" = false;
+      "Places Icons Static Size" = 22;
+    };
+
+    MainWindow.MenuBar = "Disabled";
+
+    PreviewSettings.Plugins = "appimagethumbnail,audiothumbnail,blenderthumbnail,comicbookthumbnail,cursorthumbnail,djvuthumbnail,ebookthumbnail,exrthumbnail,directorythumbnail,imagethumbnail,jpegthumbnail,kraorathumbnail,windowsexethumbnail,windowsimagethumbnail,mobithumbnail,opendocumentthumbnail,gsthumbnail,rawthumbnail,svgthumbnail,textthumbnail";
+
+    UiSettings.ColorScheme = lib.mkIf (qtStyle != null) qtStyle;
+  };
+  # }}}
 in
+# }}}
 {
-  options.custom.programs.dolphin.enable = lib.mkEnableOption "enables Dolphin";
+  # {{{ Options
+  options.custom.programs.dolphin =
+  {
+    enable = lib.mkEnableOption "enables Dolphin";
+
+    settings = lib.mkOption
+    {
+      type = lib.types.anything;
+      default = dolphinConf;
+      example = dolphinConf;
+      description = "Dolphin settings that will be placed under ~/.config/dolphinrc";
+    };
+  };
+  # }}}
+
+  # {{{ Config
   config = lib.mkIf cfg.enable
   {
-    custom.extraPackages = with pkgs.kdePackages;
-    [
-      dolphin
-      kservice
-    ];
+    custom =
+    {
+      extraPackages = with pkgs.kdePackages;
+      [
+        dolphin
+        kservice
+      ];
 
+      programs.dolphin.settings = dolphinConf;
+    };
+
+    # {{{ MIME associations fix
     ##
     ## Fixes a bug which makes the "Open With" menu empty, at the expense of
     ## pulling the entirety of plasma-workspace inside of the nix store.
@@ -26,6 +87,20 @@ in
     ## https://discourse.nixos.org/t/dolphin-does-not-have-mime-associations/48985/8
     ##
     environment.etc."/xdg/menus/applications.menu".text = builtins.readFile
-     "${workspacePkg}/etc/xdg/menus/plasma-applications.menu";
+     "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu";
+    # }}}
+
+  # {{{ Home-Manager
+  home-manager.users.${mainUser} =
+  {
+    # {{{ Config files
+    xdg.configFile =
+    {
+      "dolphinrc".text = lib.generators.toINI { } cfg.settings;
+    };
+    # }}}
   };
+  # }}}
+  };
+  # }}}
 }
